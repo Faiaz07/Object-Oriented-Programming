5a43a7674da655c9e83c7f6dcffa0a47e021748a
Faiaz Khan Soumik <66191188+Faiaz07@users.noreply.github.com>
2023-01-04 14:16:36 +0600

    Add files via upload

---
+++ Object Count/Student.java
@@ -0,0 +1,12 @@
+package statickeyword;
+
+public class Student {
+
+    static int count;
+
+    Student(){
+        count++;
+    }
+
+}
+
---
+++ Object Count/StudentTest.java
@@ -0,0 +1,15 @@
+package statickeyword;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        Student s2 = new Student();
+
+        Student s3 = new Student();
+
+        System.out.println("Object Count= "+Student.count);
+
+    }
+}
